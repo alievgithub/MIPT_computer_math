e158abfaea69dbf653d63d4113b1e5bb6e056836
Artem Aliev <artem.aliev@phystech.edu>
2021-03-23 15:52:28 +0300

    3rd point

--- First Task/ChoaticMotion3.0.py
+++ First Task/ChoaticMotion3.0.py
@@ -1,15 +1,19 @@
+''' Don't work
 from matplotlib import pyplot as plt
+from random import*
 
-def MakePlot(r, x, n):
+def MakePlot(n):
     dev_x = []
     dev_r = []
-    for i in range(n):
-        r += 1/100
-        if i > 10:
-            dev_x.append(x)
-            dev_r.append(r)
-        x = 4 * r * x * (1 - x)
-    plt.plot(dev_r, dev_x)
+    dev_r = [r / 100000 for r in range(1, 100001)]
+    for r in dev_r:
+        arr_x = []
+        x = random()
+        for i in range(n):
+            x = 4 * r * x * (1 - x)
+            arr_x.append(x)
+        dev_x.append(arr_x)
+    plt.scatter(dev_r, dev_x, 0.1)
 """
 print("Enter r, x0, n separated by space:", end = " ")
 r, x, n = input().split()
@@ -17,11 +21,30 @@ r = float(r)
 x = float(x)
 n = int(n)
 """
-MakePlot(0, 0.6, 100)
+MakePlot(2000)
 #MakePlot(0.76, 0.4, 1000)
 #MakePlot(0.8, 0.4, 1000)
 #MakePlot(0.862, 0.4, 1000)
 #MakePlot(0.8922, 0.4, 1000)
 #MakePlot(0.748, 0.4, 15)
 
+plt.show()
+'''
+
+from matplotlib import pyplot as plt
+from random import*
+
+f = lambda x: 4 * r * x * (1 - x)
+def X(n):
+    st = [f(x0)]
+    while len(st) < n:
+        st.append(f(st[-1]))
+    return st[-1]
+x = []
+dev_r = [z / 100000 for z in range(1, 100001)]
+for r in dev_r:
+    x0 = random()
+    x.append(X(2000))
+ax = plt.subplots()
+plt.scatter(dev_r, x, 0.1)
 plt.show()
